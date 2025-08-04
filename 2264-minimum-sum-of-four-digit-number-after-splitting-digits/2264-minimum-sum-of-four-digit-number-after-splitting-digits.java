827f0f384fb76f3c859b21dc3dae40bde76d5499
pranesh227 <133765321+pranesh227@users.noreply.github.com>
2025-08-04 23:26:00 +0530

    Time: 0 ms (100%), Space: 40.6 MB (54.34%) - LeetHub

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr=new int[4];
+        int i=0;
+        while(num!=0){
+            arr[i]=num%10;
+            num/=10;
+            i++;
+        }
+        Arrays.sort(arr);
+        int aa=arr[0]*10+arr[2];
+        int bb=arr[1]*10+arr[3];
+        return aa+bb;
+
+    }
+}
